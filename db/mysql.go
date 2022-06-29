63992d1beb4d6be23881dfeb2efc4da74b0f5636
李英豪 <thankslyh@gmail.com>
2022-06-29 20:43:32 +0800

    feat: 修改mysql链接

--- db/mysql.go
+++ db/mysql.go
@@ -10,7 +10,7 @@ var mysqlDB *gorm.DB
 
 func init() {
 	log.Println("init mysql")
-	dsn := "root:123456@tcp(127.0.0.1:3306)/jinkela_schema?charset=utf8&parseTime=True&loc=Local"
+	dsn := "root:123456@tcp(127.0.0.1:3307)/jinkela_schema?charset=utf8&parseTime=True&loc=Local"
 	mysqlDB, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	//if err != nil {
 	//	fmt.Println("连接错误", err.Error())
